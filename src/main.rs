fa4b88a595d09d4f13fda86a7fc28b30601a5f19
John Pothier <john.pothier@outlook.com>
2020-04-11 12:30:10 -0400

    Sort of parsing a binary expression

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 // Expand atoms
 // Evaluate
 use std::env;
+use std::error::Error;
+use std::fmt;
+use std::ops::Deref;
 
 enum Op {
     Plus,
@@ -25,9 +28,19 @@ enum Expr {
     Negate {
         e: Box<Expr>,
     },
-    Atom {
-        a: Box<Atom>,
-    },
+    Atom(Atom),
+}
+
+#[derive(Debug)]
+struct ParseError {
+    character_index: usize,
+    context: String,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error parsing expression: {}", &self.context)
+    }
 }
 
 fn print_op(op: &Op) {
@@ -46,11 +59,11 @@ fn print_atom(a: &Atom) {
 fn print_expr(e: &Expr) {
     match e {
         Expr::BinOp { op, lhs, rhs } => {
-            print_expr(&**lhs);
+            print_expr(lhs.deref());
             print_op(op);
-            print_expr(&**rhs);
+            print_expr(rhs.deref());
         }
-        Expr::Atom { a } => print_atom(a),
+        Expr::Atom(a) => print_atom(a),
         _ => println!(""),
     }
 }
@@ -68,22 +81,27 @@ fn parse_number(s: &str) -> f64 {
     return s.parse().unwrap();
 }
 
-fn parse_atom(s: &str) -> (Option<&str>, Option<Box<Atom>>) {
+fn parse_atom(s: &str) -> (&str, Option<Atom>) {
     let chars: Vec<char> = s.chars().collect();
+
+    // TODO: negative number support
+    if s.len() == 0 {
+        return (s, None);
+    }
 
     // For now only base 10 is supported
     if !chars[0].is_digit(10) {
-        return (Some(s), None);
+        return (s, None);
     }
 
     let first_non_numeric_index = chars.iter().position(|&c| !c.is_digit(10));
     if let None = first_non_numeric_index {
         return (
-            None,
-            Some(Box::new(Atom {
+            &"",
+            Some(Atom {
                 number: parse_number(s),
                 suffix: String::from(""),
-            })),
+            }),
         );
     }
 
@@ -98,23 +116,60 @@ fn parse_atom(s: &str) -> (Option<&str>, Option<Box<Atom>>) {
 
     // TODO suffix
     return (
-        Some(&s[first_non_numeric_index.unwrap()..]),
-        Some(Box::new(Atom {
+        &s[first_non_numeric_index.unwrap()..],
+        Some(Atom {
             number: parse_number(numeric_part),
             suffix: String::from(suffix_part),
-        })),
+        }),
     );
 }
 
-fn parse(s: &str) -> Expr {
-    // let mut p = -1;
+fn parse_binop(s: &str) -> (&str, Option<Op>) {
+    match s.chars().nth(0) {
+        Some('+') => (&s[1..], Some(Op::Plus)),
+        Some('-') => (&s[1..], Some(Op::Minus)),
+        Some('*') => (&s[1..], Some(Op::Times)),
+        Some('/') => (&s[1..], Some(Op::DividedBy)),
+        _ => (s, None),
+    }
+}
 
-    // for t in s.chars() {
+fn parse(user_input: &str) -> Result<Expr, ParseError> {
+    let mut p = -1;
 
-    // }
+    let mut s: &str = user_input;
+    let mut expr: Option<Expr> = None;
 
-    let (_, a) = parse_atom(s);
-    Expr::Atom { a: a.unwrap() }
+    while s.len() > 0 {
+        let (next_s, maybe_atom) = parse_atom(s);
+        s = next_s;
+        match maybe_atom {
+            None => panic!("Expected atom"),
+            Some(atom) => {
+                // See if this is part of a binary expression
+                let (next_s, maybe_binop) = parse_binop(next_s);
+                s = next_s;
+
+                if let Some(binop) = maybe_binop {
+                    let (next_s, maybe_atom) = parse_atom(s);
+                    s = next_s;
+                    expr = Some(Expr::BinOp {
+                        op: binop,
+                        lhs: Box::new(Expr::Atom(atom)),
+                        rhs: Box::new(Expr::Atom(maybe_atom.unwrap())),
+                    })
+                }
+            }
+        }
+    }
+
+    match expr {
+        None => Err(ParseError {
+            character_index: 0,
+            context: String::from(user_input),
+        }),
+        Some(e) => Ok(e),
+    }
 }
 
 fn main() {
@@ -123,6 +178,9 @@ fn main() {
         .fold(String::from(""), |s, arg| String::from(s) + &arg)
         .replace(" ", "");
 
-    let e = parse(&e_string);
-    print_expr(&e)
+    let expr_result = parse(&e_string);
+    match expr_result {
+        Err(e) => println!("{:?}", e),
+        Ok(expr) => print_expr(&expr),
+    }
 }
